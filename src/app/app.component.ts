8b6b8bf6688ad19d7a53e43b8ebe24306ee1ede8
Mario <marioperdiguero37@gmail.com>
2024-10-31 15:50:53 +0100

    🎉 Initialize project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatListItem} from '@angular/material/list';
+import {MatIcon} from '@angular/material/icon';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, MatToolbar, MatIcon, MatIconButton, MatListItem, RouterLink, MatMenu, MatMenuTrigger, MatMenuItem, MatButton, RouterLinkActive],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'ejercicios-angular-dos';
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {Exercise1Component} from '../components/firstPart/exercise1/exercise1.component';
+import {Exercise1Component as SPExercise1Component} from '../components/secondPart/exercise1/exercise1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Exercise1Component },
+  { path: 'secondPart/exercise-1', component: SPExercise1Component }
+];
